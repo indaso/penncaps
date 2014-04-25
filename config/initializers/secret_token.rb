4fc5ed98bfd7437f985a95643241ec4bd5a897d3
indaso <indaso5@gmail.com>
2014-04-25 15:16:41 -0400

    removed gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Penncaps::Application.config.secret_key_base = '7b471a590d22cbe1585e0b9516d1a8d2a2fe93ca550ec32d2f5d93529435925168fbb27be0902566ad2802a011288c5ffebd7f670076553da7cf4a5fb5af68ff'
